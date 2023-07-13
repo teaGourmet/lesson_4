3ef06d4b0340f6eb82808e2f987d80a2ec323ee2
teaGourmet <136743994+teaGourmet@users.noreply.github.com>
2023-07-13 16:49:37 +0300

    Add files via upload

---
+++ advertisements/app_advertisements/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Успешно')
+def lesson_4(request):
+    return HttpResponse('Домашка по 4 занятию')
+# Create your views here.
